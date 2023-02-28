7bdca781b894ef9d590463ead625cf094a6ad5ad
marxos121 <marxos121@gmail.com>
2023-02-28 15:43:53 +0000

    add script to display new books in DOM

--- scripts.js
+++ scripts.js
@@ -7,7 +7,27 @@ function Book(title, author, pages, haveRead) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+
+  let newBook = document.createElement("div");
+  let newBookTitle = document.createElement("p");
+  newBookTitle.textContent = book.title;
+  let newBookAuthor = document.createElement("p");
+  newBookAuthor.textContent = book.author;
+  let newBookPages = document.createElement("p");
+  newBookPages.textContent = book.pages;
+  let newBookRead = document.createElement("p");
+  newBookRead.textContent = book.haveRead ? "Read" : "Haven't read";
+
+  newBook.appendChild(newBookTitle);
+  newBook.appendChild(newBookAuthor);
+  newBook.appendChild(newBookPages);
+  newBook.appendChild(newBookRead);
+
+  libraryDisplay.appendChild(newBook);
 }
 
-let myLibrary = [new Book("Demons", "Dostoevsky", 400, true)];
+const libraryDisplay = document.querySelector(".library");
+
+let myLibrary = [];
+addBookToLibrary(new Book("Demons", "Dostoevsky", 400, true));
 addBookToLibrary(new Book("Alice in Wonderland", "Lewis Carroll", 200, false));
